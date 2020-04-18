297608d2b5a3d527b872c53194f2f4f0319a4f4f
JathushanVivekananthan <jathu.js1@gmail.com>
2020-04-19 01:31:11 +0530

    Delete patient details

--- PatientService/src/model/Patient.java
+++ PatientService/src/model/Patient.java
@@ -134,4 +134,27 @@ public class Patient { // A common method to connect to the DB
 		}
 		return output;
 	}
+
+	public String deletePatientDetails(String patientID) {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+// create a prepared statement
+			String query = "delete from patients where patientID=?";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+// binding values
+			preparedStmt.setString(1, patientID);
+// execute the statement
+			preparedStmt.execute();
+			con.close();
+			output = "Deleted successfully";
+		} catch (Exception e) {
+			output = "Error while deleting the patient detail.";
+			System.err.println(e.getMessage());
+		}
+		return output;
+	}
 }

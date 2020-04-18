b01074fb0dbaf18e0cfbfed5624efc7da192ae7e
JathushanVivekananthan <jathu.js1@gmail.com>
2020-04-19 01:34:18 +0530

    ADD patient Services

--- PatientService/src/com/PatientService.java
+++ PatientService/src/com/PatientService.java
@@ -1,5 +1,40 @@
 package com;
 
-public class PatientService {
+import model.Patient;
 
+import java.sql.Date;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/patient")
+public class PatientService {
+	Patient patientObj = new Patient();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String getPatientDetails() {
+		return patientObj.getPatientDetails();
+	}
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String addPatientDetails(@FormParam("patientID") String patientID, @FormParam("firstname") String firstname,
+			@FormParam("lastName") String lastName, @FormParam("gender") String gender,
+			@FormParam("phone") String phone, @FormParam("address") String address, @FormParam("age") String age,
+			@FormParam("bloodGroup") String bloodGroup, @FormParam("nextOfKin") String nextOfKin) {
+		String output = patientObj.addPatientDetails(patientID, firstname, lastName, gender, phone, address, age,
+				bloodGroup, nextOfKin);
+		return output;
+	}
 }

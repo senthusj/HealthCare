ca2e7d40eddb0287bcd87e7115a88b7a870e516e
senthusj <senthu231997@gmail.com>
2020-04-18 23:36:17 +0530

    calling read menthod

--- HospitalService/src/com/HospitalService.java
+++ HospitalService/src/com/HospitalService.java
@@ -1,5 +1,24 @@
 package com;
 
-public class HospitalService {
+import model.Hospital;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
 
+@Path("/Hospitals")
+public class HospitalService {
+	Hospital hospitalObj = new Hospital();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readHospitals() {
+		return hospitalObj.readHospitals();
+	}
 }

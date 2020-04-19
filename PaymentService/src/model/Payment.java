7f39c56e4a1caa5e0485be093f75cd3e0a3bc481
Kasthurykasthu <kasthu2821@gmail.com>
2020-04-19 15:43:55 +0530

    DB connection

--- PaymentService/src/model/Payment.java
+++ PaymentService/src/model/Payment.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Payment {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class Payment {
+	private Connection connect()  
+	{  
+		Connection con = null; 
+	
+		try   
+		{     
+			Class.forName("com.mysql.jdbc.Driver");          
+			//Provide the correct details: DBServer/DBName, username, password     
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/hospitalhealth","root","");   
+			}   
+		catch (Exception e)
+		{e.printStackTrace();} 
+		
+		return con;  
+	}
+	
 }

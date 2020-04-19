85175b84958a1ec2c8ed3db19a47c7a6fdb953d9
Raavan <jinthiragith@gmail.com>
2020-04-19 13:00:30 +0530

    dbcode

--- AppointmentService/src/model/Appointment.java
+++ AppointmentService/src/model/Appointment.java
@@ -1,5 +1,21 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Appointment {
 
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/test?useLegacyDatetimeCode=false&serverTimezone=UTC", "root", "");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }

4ac78dfe20d901fb5c92f5d120216ad0c15ab44a
Kasthurykasthu <kasthu2821@gmail.com>
2020-04-19 15:51:20 +0530

    Insert

--- PaymentService/src/com/PaymentService.java
+++ PaymentService/src/com/PaymentService.java
@@ -1,5 +1,39 @@
 package com;
 
-public class PaymentService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import model.Payment;
+
+@Path("/Payments")
+public class PaymentService {
+	Payment paymentObj = new Payment(); 
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertpaymentdetails(@FormParam("paymentID") String paymentID,
+							@FormParam("CARD_NUMBER") String CARD_NUMBER, 
+							@FormParam("EXPIRATIONEXP_DATE") String EXPIRATIONEXP_DATE,    
+							@FormParam("CV_CODE") String CV_CODE,       
+							@FormParam("COUPON_CODE")String COUPON_CODE) 
+	{  
+		String output = paymentObj.insertpaymentdetails(paymentID,CARD_NUMBER,EXPIRATIONEXP_DATE, CV_CODE, COUPON_CODE); 
+		return output; 
+	} 
+	
 }
--- PaymentService/src/model/Payment.java
+++ PaymentService/src/model/Payment.java
@@ -23,4 +23,44 @@ public class Payment {
 		return con;  
 	}
 	
+	public String insertpaymentdetails(String paymentID,String CARD_NUMBER, String EXPIRATIONEXP_DATE,String CV_CODE, String COUPON_CODE)  
+	{   
+		String output = ""; 
+	 
+	  try   
+	  {    
+		  Connection con = connect(); 
+	 
+		  if (con == null)
+		  {return "Error while connecting to the database for inserting."; } 
+	 
+		  // create a prepared statement    
+		  String query = " insert into payment (`payno`,`paymentID`,`CARD_NUMBER`,`EXPIRATIONEXP_DATE`,`CV_CODE`,`COUPON_CODE`)"      
+				  + " values (?, ?, ?, ?, ?, ?)"; 
+	 
+	   PreparedStatement preparedStmt = con.prepareStatement(query); 
+	 
+	   // binding values   
+	   preparedStmt.setInt(1, 0);
+	   preparedStmt.setString(2, paymentID);  
+	   preparedStmt.setString(3, CARD_NUMBER);   
+	   preparedStmt.setString(4, EXPIRATIONEXP_DATE);    
+	   preparedStmt.setString(5, CV_CODE);    
+	   preparedStmt.setString(6, COUPON_CODE); 
+	   
+	   preparedStmt.execute();    
+	   con.close(); 
+	   
+	   output = "Inserted successfully";  
+	   }  
+	  catch (Exception e)   
+	  {    
+		  output = "Error while inserting the payment details.";
+		  System.err.println(e.getMessage());  
+	  } 
+	 
+	  return output;  
+	}
+	
+	
 }

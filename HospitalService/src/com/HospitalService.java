25f33f0d315b9f6bd5128e1a449f4012340425e8
senthusj <senthu231997@gmail.com>
2020-04-19 11:20:59 +0530

    calling delete method

--- HospitalService/src/com/HospitalService.java
+++ HospitalService/src/com/HospitalService.java
@@ -55,5 +55,19 @@ public class HospitalService {
 
 		return output;
 	}
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteHospital(String hospitalData) {
+		// Convert the input string to an XML document
+		Document doc = Jsoup.parse(hospitalData, "", Parser.xmlParser());
+
+		// Read the value from the element <itemID>
+		String hospitalID = doc.select("hospitalID").text();
+		String output = hospitalObj.deleteHospital(hospitalID);
+		
+		return output;
+	}
 
 }
